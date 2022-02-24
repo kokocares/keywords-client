59931563a45c8c50d80aa669ff2cb9d8bc0e5a85
Kareem Kouddous <kkouddous@gmail.com>
2022-02-24 13:14:07 -0600

    Cache (#2)

--- src/lib.rs
+++ src/lib.rs
@@ -2,15 +2,21 @@
 // - Expire cache using cache headers or default
 // - Error handling
 // - Passing version
+// - Config for setting auth and url instead of using env vars
+// - Packaging up python lib
+// - CI/CD
+// - Tests
+// - Documentation
 
-//use std::ffi::c_void
 use std::{ffi::CStr, sync::Mutex, env, collections::HashMap};
 use lazy_static::lazy_static;
-
-const URL: &str = "api.kokocares.org/keywords";
-
 use regex::Regex;
 use serde::Deserialize;
+use cache_control::CacheControl;
+use chrono::Utc;
+
+const URL: &str = "api.kokocares.org/keywords";
+const CACHE_EXPIRATION_DEFAULT: i64 = 60;
 
 #[derive(Deserialize, Debug)]
 struct Keywords {
@@ -18,20 +24,24 @@ struct Keywords {
     pub preprocess: String,
 }
 
+struct KeywordsCache {
+    pub expires_at: i64,
+    pub keywords: Keywords
+}
+
 #[derive(Deserialize, Debug)]
 struct ApiResponse {
-    pub version: String,
     pub regex: Keywords,
 }
 
 struct KokoKeywords {
-    pub keywords: HashMap<String, Keywords>,
+    pub keywords: HashMap<String, KeywordsCache>,
     pub url: String,
 }
 
 impl KokoKeywords {
     pub fn new() -> Self {
-        let url = match (env::var("URL").ok(), env::var("AUTH").ok()) {
+        let url = match (env::var("KOKO_KEYWORDS_URL").ok(), env::var("KOKO_KEYWORDS_AUTH").ok()) {
             (Some(_), Some(_)) => panic!("AUTH and URL are mutually exclusive. Put the auth in the URL itself"),
             (Some(url), None) => url,
             (None, Some(auth)) => format!("https://{}@{}", auth, URL),
@@ -45,32 +55,53 @@ impl KokoKeywords {
     }
 
     pub fn verify(&mut self, keyword: &str, filter: &str) -> bool {
-        if let Some(regex) = self.keywords.get(filter) {
-            println!("Maching on '{}' with filter '{}'", keyword, filter);
+        let cache_key = filter;
 
-            let re = Regex::new(&regex.preprocess).unwrap();
-            let keyword = re.replace_all(keyword, "");
+        if let Some(keyword_cache) = self.keywords.get(cache_key) {
+            if Utc::now().timestamp() < keyword_cache.expires_at  {
+                let re = Regex::new(&keyword_cache.keywords.preprocess).unwrap();
+                let keyword = re.replace_all(keyword, "");
 
-            for re_keyword in &regex.keywords {
-                let re = Regex::new(re_keyword).unwrap();
-                if re.is_match(&keyword) {
-                    return true;
+                for re_keyword in &keyword_cache.keywords.keywords {
+                    let re = Regex::new(re_keyword).unwrap();
+                    if re.is_match(&keyword) {
+                        return true;
+                    }
                 }
+
+                return false
+            } else {
+                self.load_cache(cache_key);
+                self.verify(keyword, filter)
             }
-
-            false
         } else {
-            println!("Loading regex for filter '{}'", filter);
-            let r = ureq::get(&self.url)
-                .query("filter", filter)
-                .call()
-                .expect("Can't fetch");
-
-            let r: ApiResponse = serde_json::from_reader(r.into_reader()).expect("Can't parse");
-            self.keywords.insert(filter.to_string(), r.regex);
-
+            self.load_cache(cache_key);
             self.verify(keyword, filter)
         }
+    }
+
+    pub fn load_cache(&mut self, filter: &str) -> u8 {
+        println!("Loading cache for filter '{}'", filter);
+        let response = ureq::get(&self.url)
+            .query("filter", filter)
+            .call()
+            .expect("Can't fetch");
+
+        let expires_in = response.header("cache-control")
+            .map(CacheControl::from_value)
+            .flatten()
+            .map(|cc| cc.max_age)
+            .flatten()
+            .map(|max_age| max_age.as_secs() as i64)
+            .unwrap_or(CACHE_EXPIRATION_DEFAULT);
+
+        let api_response: ApiResponse = serde_json::from_reader(response.into_reader()).expect("Can't parse");
+        let keywords_cache = KeywordsCache {
+            keywords: api_response.regex,
+            expires_at: Utc::now().timestamp() + expires_in,
+        };
+        self.keywords.insert(filter.to_string(), keywords_cache);
+        0
     }
 }
 
