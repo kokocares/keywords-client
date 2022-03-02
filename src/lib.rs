54c044e4b08525568c59a1e116e0c0ecbffcc12b
Kareem Kouddous <kkouddous@gmail.com>
2022-03-01 23:16:00 -0800

    cleanup

--- src/lib.rs
+++ src/lib.rs
@@ -1,28 +1,29 @@
-// TODO: 
+// TODO:
 // - Logging
 // - CI/CD
 // - Tests
 // - RELEASE
 
-use std::{ffi::CStr, sync::Mutex, env, collections::HashMap, time::SystemTime};
+use cache_control::CacheControl;
 use lazy_static::lazy_static;
 use regex::Regex;
 use serde::Deserialize;
-use cache_control::CacheControl;
 use std::time::Duration;
-use ureq::Error;
+use std::{collections::HashMap, env, ffi::CStr, sync::Mutex, time::SystemTime};
+use ureq::{Error, ErrorKind};
 
 const URL: &str = "api.kokocares.org/keywords";
 const CACHE_EXPIRATION_DEFAULT: Duration = Duration::from_secs(3600);
 
 type KokoResult<T> = Result<T, KokoError>;
 
-#[derive(Debug, Clone, Copy)]
+#[derive(Debug, Clone, Copy, PartialEq)]
 pub enum KokoError {
-    AuthOrUrlMissing=-1,
-    InvalidCredentials=-2,
-    CacheRefreshError=-3,
-    ParseError=-4,
+    AuthOrUrlMissing = -1,
+    InvalidCredentials = -2,
+    CacheRefreshError = -3,
+    ParseError = -4,
+    InvalidUrl = -5,
 }
 
 #[derive(Deserialize, Debug)]
@@ -48,14 +49,22 @@ struct KokoKeywords {
 
 impl KokoKeywords {
     pub fn new(url: String) -> Self {
-        Self { keywords: HashMap::new(), url }
+        Self {
+            keywords: HashMap::new(),
+            url,
+        }
     }
 
-    pub fn verify(&mut self, keyword: &str, filter: &str, version: Option<&str>) -> KokoResult<bool> {
-        let cache_key = format!("{}{}", filter, version.unwrap_or_default());
+    pub fn verify(
+        &mut self,
+        keyword: &str,
+        filter: &str,
+        version: Option<&str>,
+    ) -> KokoResult<bool> {
+        let cache_key = format!("{}_{}", filter, version.unwrap_or("latest"));
 
         if let Some(keyword_cache) = self.keywords.get(&cache_key) {
-            if SystemTime::now() < keyword_cache.expires_at  {
+            if SystemTime::now() < keyword_cache.expires_at {
                 let re = Regex::new(&keyword_cache.keywords.preprocess).unwrap();
                 let keyword = re.replace_all(keyword, "");
 
@@ -78,9 +87,7 @@ impl KokoKeywords {
     }
 
     pub fn load_cache(&mut self, filter: &str, version: Option<&str>) -> KokoResult<()> {
-        let cache_key = format!("{}{}", filter, version.unwrap_or_default());
-
-        eprintln!("[koko-keywords] Loading cache for '{}'", cache_key);
+        let cache_key = format!("{}_{}", filter, version.unwrap_or("latest"));
 
         let request = ureq::get(&self.url);
 
@@ -92,22 +99,34 @@ impl KokoKeywords {
         };
 
         let response = match request.call() {
-            Ok(response) => { Ok(response) },
-            Err(Error::Status(403, _)) => { Err(KokoError::InvalidCredentials) }
-            Err(_) => { Err(KokoError::CacheRefreshError) }
+            Ok(response) => Ok(response),
+            Err(Error::Transport(tranport_error)) => {
+                if tranport_error.kind() == ErrorKind::InvalidUrl {
+                    Err(KokoError::InvalidUrl)
+                } else {
+                    Err(KokoError::CacheRefreshError)
+                }
+            },
+            Err(Error::Status(403, _)) => Err(KokoError::InvalidCredentials),
+            Err(response) => {
+                println!("{:?}", response);
+                Err(KokoError::CacheRefreshError)
+            },
         }?;
 
-        let expires_in = response.header("cache-control")
+        let expires_in = response
+            .header("cache-control")
             .map(CacheControl::from_value)
             .flatten()
             .map(|cc| cc.max_age)
             .flatten()
             .unwrap_or(CACHE_EXPIRATION_DEFAULT);
 
-        let api_response: ApiResponse = serde_json::from_reader(response.into_reader()).map_err(|_| KokoError::ParseError)?;
+        let api_response: ApiResponse =
+            serde_json::from_reader(response.into_reader()).map_err(|_| KokoError::ParseError)?;
         let keywords_cache = KeywordsCache {
             keywords: api_response.regex,
-            expires_at: SystemTime::now() + expires_in
+            expires_at: SystemTime::now() + expires_in,
         };
         self.keywords.insert(cache_key.to_string(), keywords_cache);
 
@@ -120,9 +139,11 @@ lazy_static! {
         Mutex::new(get_url().map(KokoKeywords::new));
 }
 
-
 pub fn get_url() -> KokoResult<String> {
-    match (env::var("KOKO_KEYWORDS_URL").ok(), env::var("KOKO_KEYWORDS_AUTH").ok()) {
+    match (
+        env::var("KOKO_KEYWORDS_URL").ok(),
+        env::var("KOKO_KEYWORDS_AUTH").ok(),
+    ) {
         (Some(_), Some(_)) => Err(KokoError::AuthOrUrlMissing),
         (Some(url), None) => Ok(url),
         (None, Some(auth)) => Ok(format!("https://{}@{}", auth, URL)),
@@ -131,30 +152,109 @@ pub fn get_url() -> KokoResult<String> {
 }
 
 fn koko_keywords_match_inner(input: &str, filter: &str, version: Option<&str>) -> KokoResult<bool> {
-    MATCHER.lock().unwrap().as_mut().map_err(|e| e.clone())?
+    MATCHER
+        .lock()
+        .unwrap()
+        .as_mut()
+        .map_err(|e| e.clone())?
         .verify(input, filter, version)
 }
 
+fn str_from_c<'a>(c_str: *const libc::c_char) -> Option<&'a str> {
+    if c_str.is_null() {
+        None
+    } else {
+        Some(
+            unsafe { CStr::from_ptr(c_str) }
+                .to_str()
+                .expect("Malformed UTF-8 string"),
+        )
+    }
+}
+
 #[no_mangle]
-pub extern "C" fn koko_keywords_match(input: *const libc::c_char, filter: *const libc::c_char, version: *const libc::c_char) -> isize {
+pub extern "C" fn koko_keywords_match(
+    input: *const libc::c_char,
+    filter: *const libc::c_char,
+    version: *const libc::c_char,
+) -> isize {
     let input = str_from_c(input).expect("Input is required");
     let filter = str_from_c(filter).expect("Filter is required");
     let version = str_from_c(version);
 
     let result = koko_keywords_match_inner(input, filter, version);
     match result {
-        Ok(r) => if r { 1 } else { 0 }
+        Ok(r) => {
+            if r {
+                1
+            } else {
+                0
+            }
+        }
         Err(e) => e as isize,
     }
 }
 
-pub fn str_from_c<'a>(c_str: *const libc::c_char) -> Option<&'a str> {
-    if c_str.is_null() {
-        None
-    } else {
-        Some(
-            unsafe { CStr::from_ptr(c_str) }
-                .to_str().expect("Malformed UTF-8 string")
-        )
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn test_empty_cache() {
+        let mut x = KokoKeywords {
+            keywords: HashMap::new(),
+            url: "http://localhost".to_string(),
+        };
+
+        assert_eq!(
+            x.verify("hello", "", None),
+            Err(KokoError::CacheRefreshError)
+        );
+    }
+
+    #[test]
+    fn test_invalid_url() {
+        let mut x = KokoKeywords {
+            keywords: HashMap::new(),
+            url: "".to_string(),
+        };
+
+        assert_eq!(
+            x.verify("hello", "", None),
+            Err(KokoError::InvalidUrl)
+        );
+    }
+
+    #[test]
+    fn test_unexpired_cache() {
+        let mut x = KokoKeywords {
+            keywords: HashMap::from([(
+                "_latest".to_string(),
+                KeywordsCache {
+                    keywords: Keywords {
+                        keywords: vec!["^badword$".to_string()],
+                        preprocess: " ".to_string(),
+                    },
+                    expires_at: SystemTime::now() + Duration::new(1000, 0),
+                },
+            )]),
+            url: "http://localhost".to_string(),
+        };
+
+        assert_eq!(
+            x.verify("hello", "", None),
+            Ok(false)
+        );
+
+        assert_eq!(
+            x.verify("badword", "", None),
+            Ok(true)
+        );
+
+        assert_eq!(
+            x.verify(" badword   ", "", None),
+            Ok(true)
+        );
     }
 }
